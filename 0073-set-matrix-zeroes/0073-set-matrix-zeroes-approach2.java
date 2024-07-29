91131eeddc496dac7d2a19fe99de38ddc6ab6cb0
Gourav Sharma <108645957+gorusharma00@users.noreply.github.com>
2024-07-29 08:26:45 +0530

    Time: 1 ms (80.74%), Space: 45.5 MB (59.77%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes-approach2.java
@@ -0,0 +1,44 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        boolean isRow = false;
+        boolean isCol = false;  // marker to check first row and col zero before traversing
+
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(matrix[i][j] == 0){
+                   matrix[0][j] = 0;
+                   matrix[i][0] = 0;
+                   if(i == 0) isRow = true;
+                   if(j == 0) isCol = true;
+                }
+            }
+        }
+
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n; j++){
+                if(matrix[i][0] == 0 || matrix[0][j] == 0){
+                   matrix[i][j] = 0;
+                }
+            }
+        }
+
+        // now make zero first row and col if there is zero before
+        if(isRow){
+            for(int i = 0; i < n; i++){
+                matrix[0][i] = 0;
+            }
+        }
+
+        if(isCol){
+            for(int i = 0; i < m; i++){
+                matrix[i][0] = 0;
+            }
+        }
+
+    }
+}
+
+// time => o(mn)
